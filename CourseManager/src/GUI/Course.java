37aafff12425f629765bddfdc2d0bbbbb8dbc15e
bman776 <46765260+bman776@users.noreply.github.com>
2019-03-13 23:19:23 -0600

    Simplified Course

--- CourseManager/src/GUI/Course.java
+++ CourseManager/src/GUI/Course.java
@@ -1,3 +1,5 @@
+package GUI;
+
 public class Course extends Item {
 	private double hours;
 	private double credits;
@@ -5,10 +7,10 @@ public class Course extends Item {
 	private int[] prerequisites;
 	private int[] antirequisites;
 
-	//	String type, String name, int ID, String description
-	public Course(String name, int ID, String letterID, String description, double hours, double credits, String labinfo,
+	//Constructor
+	public Course(String name, int ID, String description, int parent, double hours, double credits, String labinfo,
 				  int[] prerequisites, int[] antirequisites) {
-		super("Course", name, ID, letterID, description, null);
+		super("Course", name, ID, description, null, parent);
 		this.hours = hours;
 		this.credits = credits;
 		this.labinfo = labinfo;
@@ -16,61 +18,50 @@ public class Course extends Item {
 		this.antirequisites = antirequisites;
 	}
 
+	//Getters and Setters
 	public double getHours() {
 		return hours;
 	}
-
 
 	public void setHours(double hours) {
 		this.hours = hours;
 	}
 
-
 	public double getCredits() {
 		return credits;
 	}
-
 
 	public void setCredits(double credits) {
 		this.credits = credits;
 	}
 
-
 	public String getLabinfo() {
 		return labinfo;
 	}
-
 
 	public void setLabinfo(String labinfo) {
 		this.labinfo = labinfo;
 	}
 
-
 	public int[] getPrerequisites() {
 		return prerequisites;
 	}
-
 
 	public void setPrerequisites(int[] prerequisites) {
 		this.prerequisites = prerequisites;
 	}
 
-
 	public int[] getAntirequisites() {
 		return antirequisites;
 	}
 
-
 	public void setAntirequisites(int[] antirequisites) {
 		this.antirequisites = antirequisites;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Course [hours=" + hours + ", credits=" + credits + ", labinfo=" + labinfo + ", prerequisites="
 				+ prerequisites + ", antirequisites=" + antirequisites + "]";
 	}
-
-
 }
