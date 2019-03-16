2cf42ace53bce13092c672bf2ed7204c2e1b4cea
MikleM20 <40878852+MikleM20@users.noreply.github.com>
2019-03-15 18:19:49 -0600

    Update Login for Change Username/PW functionality

--- CourseManager/src/Data/Login.java
+++ CourseManager/src/Data/Login.java
@@ -11,13 +11,17 @@ import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
-
+/**
+ * Login Class used for Course and Program Manager Application
+ * @author Michael Manila
+ *
+ */
 public class Login {
 
 	private String encryptedUser;
 	private String encryptedPass;
-	public String decryptedUser;
-	public String decryptedPass;
+	private String decryptedUser;
+	private String decryptedPass;
 	
 	private PrintWriter outputStream = null;
 	
@@ -28,12 +32,16 @@ public class Login {
 	 */
 	public Login() {	
 		getCredentialsFromFile();
+		/*
+		* Prints To Help Check What Username and Password is
 		System.out.println(decryptedUser);
 		System.out.print(decryptedPass);
+		*/
 	}
 	
 	/**
-	 * 
+	 * Method used to check if user inputed proper username and login at login page
+	 * @return boolean, true if correct username and password, false otherwise
 	 */
 	public boolean loginCheck(String username, String password) {
 		if (username.equals(this.decryptedUser)) {
@@ -62,18 +70,22 @@ public class Login {
 		//System.out.println(log.getDeencryptedPass());
 		}*/
 	
+	/**
+	 * Method used to encrypt username and password using AES cipher
+	 * @param usernameToEncrypt, username to be encrypted
+	 * @param passwordToEncrypt, password to be encrypted
+	 */
 	public void encrypt(String usernameToEncrypt, String passwordToEncrypt) {
 		
 		try {
-        	String username = "admin";
             String usernameKey = "keyUsernamePlace"; // 128 bit key
             // Create key and and cipher used to encrypt
             Key aesKeyUser = new SecretKeySpec(usernameKey.getBytes(), "AES");
             Cipher cipher = Cipher.getInstance("AES");
             // encrypt the text
             cipher.init(Cipher.ENCRYPT_MODE, aesKeyUser);
-            byte[] encryptedUsername = cipher.doFinal(username.getBytes());
-            this.encryptedUser = new String(encryptedUsername);
+            byte[] encryptedUsername = cipher.doFinal(usernameToEncrypt.getBytes());
+            setEncryptedUser(new String(encryptedUsername));
             //System.out.println(new String(encryptedUser));
         }
         catch(Exception e) 
@@ -89,8 +101,8 @@ public class Login {
             Cipher cipher = Cipher.getInstance("AES");
             // encrypt the text
             cipher.init(Cipher.ENCRYPT_MODE, aesKeyPass);
-            byte[] encryptedPassword = cipher.doFinal(password.getBytes());
-            this.encryptedPass = new String(encryptedPassword);
+            byte[] encryptedPassword = cipher.doFinal(passwordToEncrypt.getBytes());
+            setEncryptedPass(new String(encryptedPassword));
             //System.out.println(new String(encryptedPass));
         }
         
@@ -101,12 +113,81 @@ public class Login {
     }
 	
 	/**
-	 * Reads Login.txt file and set the user name and pass variables
+	 * Method that changes the username, updates files and variables after changes.
+	 * @param current, username before change attempt.
+	 * @param newUsername, username that user wishes to change to.
+	 * @param repeat, username user wishes to change to.
+	 * @return int, if 0 then incorrect currentPassword, if 1, then newUsername
+	 * 			is less than 5 characters. If 2, new does not match repeat, and
+	 * 			if 3 then username is changed.
+	 */
+	public int changeUsername(String current, String newUsername, String repeat) {
+		int changeStatus;
+		if (current.equals(getDeencryptedUser()) == true){	
+			if(newUsername.length() >= 5) {
+				if (newUsername.equals(repeat) == true) {
+					setDeencryptedUser(newUsername);
+					encrypt(newUsername, this.decryptedPass);
+					updateFile();
+					getCredentialsFromFile();
+					changeStatus = 3;
+				}
+				else {
+					changeStatus = 2;
+				}
+			}
+			else {
+				changeStatus = 1;
+			}
+		}
+		else {
+			changeStatus = 0;
+		}
+		return changeStatus;
+	}
+
+	/**
+	 * Method that changes the password, updates files and variables after changes.
+	 * @param current, password before change attempt.
+	 * @param newPw, password that user wishes to change to.
+	 * @param repeatPW, password user wishes to change to.
+	 * @return int, if 0 then incorrect current password, if 1, then newPW
+	 * 			is less than 7 characters. If 2, new does not match repeat, and
+	 * 			if 3 then password is changed.
+	 */
+	public int changePassword(String current, String newPW, String repeatPW) {
+		int changeStatus;
+		if (current.equals(getDeencryptedPass()) == true){	
+			if(newPW.length() >= 7) {
+				if (newPW.equals(repeatPW) == true) {
+					setDeencryptedPass(newPW);
+					encrypt(this.decryptedUser, newPW);
+					updateFile();
+					getCredentialsFromFile();
+					changeStatus = 3;
+				}
+				else {
+					changeStatus = 2;
+				}
+			}
+			else {
+				changeStatus = 1;
+			}
+		}
+		else {
+			changeStatus = 0;
+		}
+		return changeStatus;
+	}
+	
+	/**
+	 * Reads Login.txt file and set the username and password variables
 	 */
 	public void getCredentialsFromFile() {
 		BufferedReader reader;
 		try {
 			
+			// Read first two lines and set them as username and password
 			reader = new BufferedReader(new FileReader(filePath));
 			setEncryptedUser(reader.readLine());
 			setEncryptedPass(reader.readLine());	
@@ -139,11 +220,21 @@ public class Login {
 		catch(Exception e) 
         {
             e.printStackTrace();
-        }
-		
-		
+        }	
 	}
-		
+	
+	/**
+	 * Updates the Ligin.txt file with current encrypted user and pass Strings
+	 */
+	public void updateFile() {
+        try{
+            outputStream = new PrintWriter(new FileOutputStream("./data/Login.txt" , false));
+            outputStream.println(this.getEncryptedUser());
+            outputStream.print(this.getEncryptedPass());
+            outputStream.close();
+        }
+        catch(FileNotFoundException e) {}
+	}
 	
 	/**
 	 * Setter for encryptedUser
@@ -177,7 +268,6 @@ public class Login {
 		this.decryptedPass = decrypted;
 	}
 	
-	
 
 	/**
 	 *  Getter for encryptedUser
@@ -209,19 +299,6 @@ public class Login {
 	 */
 	public String getDeencryptedPass() {
 		return decryptedPass;
-	}
-		
-	/**
-	 * Updates the Ligin.txt file with current encrypted user and pass Strings
-	 */
-	public void updateFile() {
-        try{
-            outputStream = new PrintWriter(new FileOutputStream("./data/Login.txt" , false));
-            outputStream.println(this.getEncryptedUser());
-            outputStream.print(this.getEncryptedPass());
-            outputStream.close();
-        }
-        catch(FileNotFoundException e) {}
 	}
 	
 	
