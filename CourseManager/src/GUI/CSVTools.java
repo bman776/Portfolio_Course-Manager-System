89a05e77b8e285d48b1beed3a772f8d229a3b96e
ss <>
2019-04-05 14:54:52 -0600

    Add Java doc for CSVTools

--- CourseManager/src/GUI/CSVTools.java
+++ CourseManager/src/GUI/CSVTools.java
@@ -8,11 +8,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class is to read and write CSV files. 
+ * The CSV files can be read and write using text editor directly, but we suggest to use 
+ * this tool. Because editing directly may destroy the form of csv files. 
+ * @author Brett Gattinger, Shiwei Sun
+ * 
+ */
+
 public class CSVTools {
 
     private static final char DEFAULT_SEPARATOR = ',';
     private static final char DEFAULT_QUOTE = '"';
-
+    /**
+    * This function is used to find one item by its name. It will read the csv file, and return
+    * the corresponding item instance.
+    * @param type This indicate the type of item you want to find. for example, "department" 
+    * @param name This is the name of the item you want to find.
+    * @return The instance of the item we want to find.
+    */
     public static Item findItem(String type, String name) {
     	String csvFile = "CourseManager/data/"+type+".csv";
     	Item result = null;
@@ -48,7 +62,13 @@ public class CSVTools {
         }
     	return result;	
     }
-    
+    /**
+     * This function is used to find one item by its ID. It will read the csv file, and return
+    * the corresponding item instance.
+    * @param type This indicate the type of item you want to find. for example, "department" 
+    * @param ID This is the name of the item you want to find.
+     * @return The instance of the item we want to find.
+    */
     public static Item findItem(String type, int ID) {
     	String csvFile = "CourseManager/data/"+type+".csv";
     	Item result = null;
@@ -84,7 +104,12 @@ public class CSVTools {
         }
     	return result;	
     }
-
+    /**
+     * This function is to find the course by its name. It will return the instance of
+     * corresponding course.
+     * @param name The name of the course you want to find.
+     * @return The instance of that course.
+     */
     public static Course findCourse(String name) {
     	String csvFile = "CourseManager/data/course.csv";
     	Course result = null;
@@ -126,7 +151,12 @@ public class CSVTools {
         }
     	return result;	
     }
-    
+    /**
+     * This function is to find the course by its ID. It will read the csv file first and 
+     * return the instance of corresponding course.
+     * @param ID The name of the course you want to find.
+     * @return The instance of that course.
+     */
     public static Course findCourse(int ID) {
     	String csvFile = "CourseManager/data/course.csv";
     	Course result = null;
@@ -168,7 +198,12 @@ public class CSVTools {
         }
     	return result;	
     }
-    
+    /**
+     * This item will take an item parameter, and it will write this item to the corresponding 
+     * csv file. For example, if the type of the para is department, this function will write an
+     * department details in department.csv. 
+     * @param newitem The instance of item you want to add to the corresponding csv file.
+     */
     public static void addItem(Item newitem) {
     	String csvFile = "CourseManager/data/"+newitem.type+".csv";
     	Scanner scanner = null;
@@ -215,7 +250,11 @@ public class CSVTools {
             }
         }
     }
-    
+    /**
+     * This item will take an course parameter, and it will write this item to the corresponding 
+     * csv file. For example, 
+     * @param newcourse The instance of item you want to add to course.
+     */
     public static void addCourse(Course newcourse) {
     	String csvFile = "CourseManager/data/course.csv";
     	Scanner scanner = null;
@@ -291,7 +330,13 @@ public class CSVTools {
             }
         }
     }
-    
+    /**
+     * This function is used to add child to parent item and write to the parent csv file.
+     * For example, if you want to add a new course, you should not only write the coure details 
+     * to the course csv file. You should also add the course ID to the program file, as course if
+     * the child of program.
+     * @param child The child instance.
+     */
     public static void addChildIDtoParent(Item child){
         Item parentItem = null;
         if (child.type.equals("course")) {
@@ -311,7 +356,11 @@ public class CSVTools {
             addItem(parentItem);
         }
     }
-
+    /**
+     * This item is used to delete one item and reove data from csv file. In this function,
+     * it can also delete data of course. Because course is inherented from item.
+     * @param deleteditem The item you want to delete.
+     */
     public static void removeData(Item deleteditem) {
     	String csvFile = "CourseManager/data/"+deleteditem.type+".csv";
     	Scanner scanner = null;
@@ -354,7 +403,13 @@ public class CSVTools {
             }
         }
     }
-    
+    /**
+     * If you want to show the IDs of all item which are of same type. You can use this 
+     * function. For example, if you use getIDList(department), you will get the IDs of all
+     * department.
+     * @param type The item type.
+     * @return The list of ID you want.
+     */
     public static int[] getIDList(String type) {
     	int[] list = null;
     	String csvFile = "CourseManager/data/"+type+".csv";
@@ -384,7 +439,12 @@ public class CSVTools {
         }
     	return list;
     }
-
+    /**
+     * This function is like getIDList(). However, this function accept a string param, 
+     * it will return a list of names of items you want to find.
+     * @param type The type of the items.
+     * @return A list of names
+     */
     public static String[] getNameList(String type) {
         String[] list = null;
         String csvFile = "CourseManager/data/"+type+".csv";
@@ -414,7 +474,7 @@ public class CSVTools {
         }
         return list;
     }
-
+    
     public static boolean fileIsEmpty(String type){
         String csvFile = "CourseManager/data/"+type+".csv";
         Scanner scanner = null;
