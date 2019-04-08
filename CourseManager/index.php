7ea9ddc02a70291d631db3cbc3f816943ed4effa
carbon56 <47364938+carbon56@users.noreply.github.com>
2019-04-08 16:10:40 -0600

    Added multiple prerequisite support

--- CourseManager/index.php
+++ CourseManager/index.php
@@ -83,8 +83,50 @@ for ($k = 2; $k < count($depData); $k++){
     echo '<a href="index.php?depInput='.$depData[$k][0].'">'.$depData[$k][1]. '</a>';
 }
 
-
+$progFile = file('data/program.csv'); // Get csv file
+$progData = [];
+// Use for loop to iterate through each line of the csv file 
+foreach ($progFile as $line){
+// This an multi-dimension array, [x][y] where x is line # and y is value in comma separated line
+// course table headers are [id, name, description, # prereq, # antireq, lab info, hours, credits, parent id, prereq, antireq]
+$progData[] = str_getcsv($line); // convert csv field data in each line to an array then push this array to array $progData
+}  // Will be used to retrieve programs later
+echo '<c href= #>Programs</c>';
+echo '<a href="index.php?progInput=Programs">View Programs</a>';
 echo '</div>';
+
+if($_GET["progInput"] == 'Programs'){ // if the view programs link is pressed, if statement is true 
+
+echo ('
+<div class="content">
+<br>
+<table style="width:100%">
+  <tr>
+    <th align="left" >Program Name</th>
+    <th align="left">Description</th> 
+  </tr>');
+
+
+  for ($i = 2; $i<= count($progData); $i++ ){
+    // skip two lines again, skip the metadata and blank line
+    // iterate over all lines in $progData
+    echo('<tr>'); // start table row
+    echo '<div>'; // start div
+      for ($q = 1; $q <= 2; $q++){
+          // skip id in $q = 1 and ignore the parent id, child id in $q = 3 and $q = 4
+
+    echo("<td>". $progData[$i][$q]. "</td>");   // use for loop to print all data
+      }
+
+      echo '</div>'; // close div
+      echo('</tr>'); // close table row 
+
+
+  }
+
+}
+
+else{
 echo ('
 <div class="content">
 <br>
@@ -95,8 +137,8 @@ echo ('
     <th align="left">Lab Info </th>
     <th align="left">Hours </th>
     <th align="left">Credits </th>
-    <th align="left">Prequisite </th>
     <th align="left">Antirequisite</th>
+    <th align="left">Prequisite(s) </th>
   </tr>');
   echo('<tr>');
   echo '<div>'; 
@@ -112,17 +154,10 @@ echo ('
 //echo '<div class="content">';
 if(isset($_GET["depInput"])){ 
     // Ensure that department # is not empty - since # is used to sort courses 
-    $depInput = $_GET["depInput"];
+    $depInput = $_GET["depInput"]; // get the user input when link is pressed, put in $depInput
 
 
-    $progFile = file('data/program.csv'); // Get csv file
-    $progData = [];
-    // Use for loop to iterate through each line of the csv file 
-    foreach ($progFile as $line){
-    // This an multi-dimension array, [x][y] where x is line # and y is value in comma separated line
-    // course table headers are [id, name, description, # prereq, # antireq, lab info, hours, credits, parent id, prereq, antireq]
-    $progData[] = str_getcsv($line); // convert csv field data in each line to an array then push this array to array $progData
-}
+
     // Start at $j = 2 since there is metadata and empty line in csv files (so skip 2 lines)
     for ($j = 2; $j <= count($progData); $j++){
         if($progData[$j][3] == $depInput){
@@ -137,43 +172,45 @@ if(isset($_GET["depInput"])){
                         // skip # prereq (x = 3), # antireq (x = 4), skip prereq and antireq (x = 8, 9)
                     echo("<td>". $classData[$y][$x]. "</td>");
                     }
-                    $prereqNum = $classData[$y][3];
 
-                    if( $x == 9){
-                        $prereq = $classData[$y][9]; // get the prereq (Course ID)
-                        if($prereq != null){
-                        // Ensure prereq is not null
-                        for ($n = 2; $n <= count($classData); $n++){
-                        // skip the first 2 lines again, find course id of prereq
-                            if($prereq == $classData[$n][0]){
-                                // Print the course name of prereq
-                                echo("<td>". $classData[$n][1]. "</td>");
-                            }
-                        }
-                    }else{ // if null - print None
-                        echo("<td>". None. "</td>");}
-        
-                    
-        
-                    }
-                    if($x == 10){
-                        $antireq = $classData[$y][10]; // get antireq (Course ID)
-                        if($antireq != null){
-                        // Ensure antireq is not null
+                    $preNum = $classData[$y][3];  // get number of prerequisites 
+
+                    if($x == 9 + $preNum){ // 9 + # of prereq is where antirequisites start, since # of prereq starts at 9 (assume at least 1, so reduce # prereq by 1)
+                        $antireq = $classData[$y][9 + $preNum]; // get course id of antireq
+                        if($antireq != null){ // ensure antireq is not null
                             for ($n = 2; $n <= count($classData); $n++){
-                            // skip the first 2 lines again, find course id of antireq
-                                if($antireq == $classData[$n][0]){
-                                    // Print the course name of antireq
-                                    echo("<td>". $classData[$n][1]. "</td>");
+                                // skip first 2 lines, retrieve the courses 
+                                if($antireq == $classData[$n][0]){ // Match the ID
+                                    echo("<td>". $classData[$n][1]. "</td>"); // Find antirequisite name, print in table
                                 }
                             }
                         }
-                        
-                        else{ // if null - print None 
+                        else{ // if null print None
                                 echo("<td>". None. "</td>");
         
                         }
-                    }
+        
+        
+                        if ($preNum == 0){ // Need at least one space for prerequisite to hold "None" 
+                            $preNum = 1;
+                        }
+                        for ($z = 9; $z < 9 + $preNum; $z++){
+                            $prereq = $classData[$y][$z];
+                            if($prereq != null){ // ensure prereq is not null 
+                            for ($n = 2; $n <= count($classData); $n++){
+                                // skip first 2 lines, retrieve the courses
+                                if($prereq == $classData[$n][0]){
+                                    echo("<td>". $classData[$n][1]. "</td>"); // Find prerequisite name, print in table
+                                }
+                            }
+                        }else{// if null print None
+                            echo("<td>". None. "</td>");}
+            
+                         }
+                        
+                    }  
+
+
                 }
                     
                     }
@@ -193,27 +230,15 @@ if(isset($_GET["depInput"])){
             // skip # prereq (x = 3), # antireq (x = 4), skip prereq and antireq (x = 8, 9)
             echo("<td>". $classData[$f][$s]. "</td>");
             }
-            if( $s == 9){
-                $prereq = $classData[$f][9];
-                if($prereq != null){ // ensure prereq is not null 
-                for ($n = 2; $n <= count($classData); $n++){
-                    // skip first 2 lines, retrieve the courses
-                    if($prereq == $classData[$n][0]){
-                        echo("<td>". $classData[$n][1]. "</td>");
-                    }
-                }
-            }else{
-                echo("<td>". None. "</td>");}
+            $preNum = $classData[$f][3]; // get the # of prereq for course in row f 
 
-            // }
-            }
-            if($s == 10){
-                $antireq = $classData[$f][10]; // get course id of antireq
+            if($s == 9 + $preNum){ // 9 + # of prereq is where antirequisites start, since # of prereq starts at 9 (assume at least 1, so reduce # prereq by 1)
+                $antireq = $classData[$f][9 + $preNum]; // get course id of antireq
                 if($antireq != null){ // ensure antireq is not null
                     for ($n = 2; $n <= count($classData); $n++){
                         // skip first 2 lines, retrieve the courses 
-                        if($antireq == $classData[$n][0]){
-                            echo("<td>". $classData[$n][1]. "</td>");
+                        if($antireq == $classData[$n][0]){ // match by ID
+                            echo("<td>". $classData[$n][1]. "</td>"); // Find the antirequisite name, print in table
                         }
                     }
                 }
@@ -221,7 +246,30 @@ if(isset($_GET["depInput"])){
                         echo("<td>". None. "</td>");
 
                 }
-            }      
+
+
+              
+                if ($preNum == 0){ // Need 1 space for none if there are no prerequisites 
+                    $preNum = 1;
+                }
+                for ($z = 9; $z < 9 + $preNum; $z++){
+                    // prereq start at column 9 ends at < 9 + # of prereq
+                    $prereq = $classData[$f][$z]; // f is the row in the table
+                    if($prereq != null){ // ensure prereq is not null 
+                    for ($n = 2; $n <= count($classData); $n++){
+                        // skip first 2 lines, retrieve the courses
+                        if($prereq == $classData[$n][0]){ // match by ID
+                            echo("<td>". $classData[$n][1]. "</td>"); // Find prerequisite, print in table
+                        }
+                    }
+                }else{// if null print none
+                    echo("<td>". None. "</td>");}
+    
+                 }
+                
+            }  
+
+    
             }
             echo ('</tr></div>');
     }
@@ -229,6 +277,9 @@ if(isset($_GET["depInput"])){
 
 echo ('</tr>    
 </table></div>'); // close table row, table, div
+}
+
+
 ?>
 </body>
 </html>
